0139f537505ed2dadb56b10325a9791ae5042405
Punkinut <heypunkinut@gmail.com>
2021-08-21 10:21:50 +1000

    Setup basic firebase app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,26 @@ import './styles/global.scss'
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCPeGT3Hz2_DG7x5K1gE-Bw1o1eg383EmQ",
+  authDomain: "discord-punkinut.firebaseapp.com",
+  projectId: "discord-punkinut",
+  storageBucket: "discord-punkinut.appspot.com",
+  messagingSenderId: "1091788640272",
+  appId: "1:1091788640272:web:435668d5eee9701310fa95",
+  measurementId: "G-N1DH7BK1BY"
+});
+
 function App() {
+  const auth = firebase.auth();
+  const [user] = useAuthState(auth);
   return (
     <>
       <Router>
@@ -19,3 +38,4 @@ function App() {
 }
 
 export default App;
+
