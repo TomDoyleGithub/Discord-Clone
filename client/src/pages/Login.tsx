87f0865c0efbdb508c0a03ec4650231c870d3ac7
Punkinut <heypunkinut@gmail.com>
2021-08-21 15:53:02 +1000

    Quick styling commit

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -8,7 +8,10 @@ function Login({user}) {
         <div className='fullscreen'>
             <img className='backdrop-img' src={backdrop} alt='Backdrop'/>
             <section className='login-container'>
-                
+                <form className='form-container'>
+                    <p>Welcome Back!</p>
+                    <p>We're so excited to see you again!</p>
+                </form>
             </section>
         </div>
     )
