3557a0ce301a1c3316bde8e83463bffeb1ebdbae
Punkinut <heypunkinut@gmail.com>
2021-08-22 12:41:04 +1000

    Created basic styling for login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -7,8 +7,18 @@ function Login() {
             <img className='backdrop-img' src={backdrop} alt='Backdrop'/>
             <section className='login-container'>
                 <form className='form-container'>
-                    <p>Welcome Back!</p>
-                    <p>We're so excited to see you again!</p>
+                    <p className='header-font f600' style={{fontSize: "25px"}}>Welcome back!</p>
+                    <p className='normal-font f300' style={{opacity: 0.7}}>We're so excited to see you again!</p>
+                    <div className='input-container'>
+                        <label className='normal-font f500 login-label' style={{fontSize: "12px", opacity: 0.7}}>EMAIL</label>
+                        <input name='email' className='input'/>
+                    </div>
+                    <div className='input-container'>
+                        <label className='normal-font f500 login-label' style={{fontSize: "12px", opacity: 0.7}}>PASSWORD</label>
+                        <input name='password' className='input'/>
+                    </div>
+                    <button className='form-button normal-font' style={{marginTop: '20px'}}>Login</button>
+                    <p className='normal-font f300' style={{opacity: 0.5, fontSize: "14px"}}>Need an account? Register</p>
                 </form>
             </section>
         </div>
