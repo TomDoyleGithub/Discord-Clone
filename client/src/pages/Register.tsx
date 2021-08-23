652336c2f89ef3d85a57abcf86f6ca4a27d6c6b8
Punkinut <heypunkinut@gmail.com>
2021-08-23 14:19:46 +1000

    Added years to code

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,6 +4,18 @@ import { Link } from 'react-router-dom'
 import backdrop from '../images/AuthBackground.svg'
 
 function Register() {
+    function generateArrayOfYears() {
+        const max = new Date().getFullYear()
+        const min = max - 150
+        const years:any = []
+      
+        for (let i:number = max; i >= min; i--) {
+          years.push(i)
+        }
+        return years
+      }
+      
+    const years = generateArrayOfYears();
     const [mmActive, setmmActive] = useState(false);
     const [ddActive, setddActive] = useState(false);
     const [yyActive, setyyActive] = useState(false);
@@ -79,11 +91,47 @@ function Register() {
                                 <input name='mm' onClick={handleClick} className='input normal-font f300' placeholder='Select' style={{width: '150px', cursor: 'default'}}/>
                             </section>
                             <section className='full-dropdown'>
-                                <section className={ddActive ? 'selection' : 'none'} style={{width: '100px'}}></section>
+                                <section className={ddActive ? 'selection' : 'none'} style={{width: '100px'}}>
+                                    <p>1</p>
+                                    <p>2</p>
+                                    <p>3</p>
+                                    <p>4</p>
+                                    <p>5</p>
+                                    <p>6</p>
+                                    <p>7</p>
+                                    <p>8</p>
+                                    <p>9</p>
+                                    <p>10</p>
+                                    <p>11</p>
+                                    <p>12</p>
+                                    <p>13</p>
+                                    <p>14</p>
+                                    <p>15</p>
+                                    <p>16</p>
+                                    <p>17</p>
+                                    <p>18</p>
+                                    <p>19</p>
+                                    <p>20</p>
+                                    <p>21</p>
+                                    <p>22</p>
+                                    <p>23</p>
+                                    <p>24</p>
+                                    <p>25</p>
+                                    <p>26</p>
+                                    <p>27</p>
+                                    <p>28</p>
+                                    <p>29</p>
+                                    <p>30</p>
+                                    <p>31</p>
+                                </section>
                                 <input name='dd' onClick={handleClick} className='input normal-font f300' placeholder='Select' style={{width: '100px', cursor: 'default'}}/>
                             </section>
                             <section className='full-dropdown'>
-                                <section className={yyActive ? 'selection' : 'none'}  style={{width: '122px'}}></section>
+                                <section className={yyActive ? 'selection' : 'none'}  style={{width: '122px'}}>
+                                    {years?.map((item:string , i:number) => (
+                                        <p key={i}>{item}</p>
+                                    ))}
+                                </section>
                                 <input name='yy' onClick={handleClick} className='input normal-font f300' placeholder='Select' style={{width: '120px', cursor: 'default'}}/>
                             </section>
                         </div>
