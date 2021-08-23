0d810e85daa54812113ac242aeeccb470e6a311c
Punkinut <heypunkinut@gmail.com>
2021-08-23 15:44:47 +1000

    Converted dob values into arrays to map over in JSX

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react'
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom'
 import backdrop from '../images/AuthBackground.svg'
-
 function Register() {
+    const days = Array.from({length: 31}, (_, index) => index + 1);
+    const months = Array.from({length: 12}, (item, i) => {
+        return new Date(0, i).toLocaleString('en-US', {month: 'long'})
+      });
     function generateArrayOfYears() {
         const max = new Date().getFullYear()
         const min = max - 150
@@ -75,54 +78,17 @@ function Register() {
                         <div className='dropdown-container'>
                             <section className='full-dropdown'>
                                 <section className={mmActive ? 'selection' : 'none'} style={{width: '150px'}}>
-                                    <p>January</p>
-                                    <p>February</p>
-                                    <p>March</p>
-                                    <p>April</p>
-                                    <p>May</p>
-                                    <p>June</p>
-                                    <p>July</p>
-                                    <p>August</p>
-                                    <p>September</p>
-                                    <p>October</p>
-                                    <p>November</p>
-                                    <p>December</p>
+                                {months.map((month, i) => (
+                                        <p key={i}>{month}</p>
+                                    ))}
                                 </section>
                                 <input name='mm' onClick={handleClick} className='input normal-font f300' placeholder='Select' style={{width: '150px', cursor: 'default'}}/>
                             </section>
                             <section className='full-dropdown'>
                                 <section className={ddActive ? 'selection' : 'none'} style={{width: '100px'}}>
-                                    <p>1</p>
-                                    <p>2</p>
-                                    <p>3</p>
-                                    <p>4</p>
-                                    <p>5</p>
-                                    <p>6</p>
-                                    <p>7</p>
-                                    <p>8</p>
-                                    <p>9</p>
-                                    <p>10</p>
-                                    <p>11</p>
-                                    <p>12</p>
-                                    <p>13</p>
-                                    <p>14</p>
-                                    <p>15</p>
-                                    <p>16</p>
-                                    <p>17</p>
-                                    <p>18</p>
-                                    <p>19</p>
-                                    <p>20</p>
-                                    <p>21</p>
-                                    <p>22</p>
-                                    <p>23</p>
-                                    <p>24</p>
-                                    <p>25</p>
-                                    <p>26</p>
-                                    <p>27</p>
-                                    <p>28</p>
-                                    <p>29</p>
-                                    <p>30</p>
-                                    <p>31</p>
+                                    {days.map((day, i) => (
+                                        <p key={i}>{day}</p>
+                                    ))}
                                 </section>
                                 <input name='dd' onClick={handleClick} className='input normal-font f300' placeholder='Select' style={{width: '100px', cursor: 'default'}}/>
                             </section>
