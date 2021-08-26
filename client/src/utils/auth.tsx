615c72fcf443590b6c577cd23d16fd19e7d064c4
Punkinut <heypunkinut@gmail.com>
2021-08-26 12:46:22 +1000

    Added front-end auth

---
+++ client/src/utils/auth.tsx
@@ -0,0 +1,40 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    const token:any = this.getToken();
+    return decode(token);
+  }
+
+  loggedIn() {
+    const token:any = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token: any) {
+    try {
+      const decoded:any = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken: any) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
