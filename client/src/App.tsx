4cd9f52f774964fd41b5622ea48a13b388303095
Punkinut <heypunkinut@gmail.com>
2021-08-26 17:00:00 +1000

    Implemented basic login form states and apollo client to front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './styles/reset.sass'
 import './styles/global.scss'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
   
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
@@ -20,7 +38,7 @@ function App() {
           <Route exact path='/register' component={Register} />
         </Switch>
       </Router>
-    </>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { useMutation } from '@apollo/client';
+import React, { useState } from 'react'
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom'
 import backdrop from '../images/AuthBackground.svg'
 import logo from '../images/Discord-Logo-White.svg'
+import Auth from '../utils/auth';
+import { LOGIN } from '../utils/mutations';
 
 function Login() {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    console.log(formState);
+    const [login] = useMutation(LOGIN);
+
+    const handleChange = (event: any) => {
+        const { name, value } = event.target;
+        setFormState({...formState, [name]: value });
+      };
+
+    const handleFormSubmit = async (event: any) => {
+        event.preventDefault();
+
+        try {
+            const mutationResponse = await login({ variables: {email: formState.email, password: formState.password,}})
+            const token = mutationResponse?.data?.login?.token;
+            Auth.login(token)
+        } catch (err) {
+            console.log('Something went wrong...')
+        }
+    }
     return (
         <div className='fullscreen'>
             <img className='backdrop-img' src={backdrop} alt='Backdrop'/>
@@ -16,11 +39,11 @@ function Login() {
                     <p className='normal-font f300' style={{opacity: 0.7}}>We're so excited to see you again!</p>
                     <div className='input-container'>
                         <label className='normal-font f500 login-label' style={{fontSize: "12px", opacity: 0.7}}>EMAIL</label>
-                        <input type='email' name='email' className='input normal-font f300'/>
+                        <input onChange={handleChange} type='email' name='email' className='input normal-font f300'/>
                     </div>
                     <div className='input-container'>
                         <label className='normal-font f500 login-label' style={{fontSize: "12px", opacity: 0.7}}>PASSWORD</label>
-                        <input type='password' name='password' className='input normal-font f300'/>
+                        <input onChange={handleChange} type='password' name='password' className='input normal-font f300'/>
                     </div>
                     <button className='form-button normal-font' style={{marginTop: '20px'}}>Login</button>
                     <p className='normal-font f300' style={{opacity: 0.5, fontSize: "14px"}}>Need an account? <Link className='link f400' to='/register'>Register</Link> </p>
