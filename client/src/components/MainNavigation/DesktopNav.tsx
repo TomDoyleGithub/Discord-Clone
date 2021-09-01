fa6789bf121f8252f243e15ce80dd19fd45b6c30
Punkinut <heypunkinut@gmail.com>
2021-09-01 12:43:03 +1000

    Added triangle to speech bubble

--- client/src/components/MainNavigation/DesktopNav.tsx
+++ client/src/components/MainNavigation/DesktopNav.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './navigation.scss';
 import Auth from '../../utils/auth';
 import { IoAddSharp, IoCompassSharp } from 'react-icons/io5';
+import { AiOutlineCaretRight } from 'react-icons/ai';
 import logo from '../../images/Discord-White.svg';
 import { NavLink } from 'react-router-dom';
 
@@ -14,20 +15,29 @@ function DesktopNav() {
         <div className='border left-side-nav-container'>
             <NavLink exact activeClassName='active-discord' to='/' className={'nav-circle purple-back'}>
                 <section className='white-strip'></section>
-                <div className='nav-speech-bubble normal-font f500'>Home</div>
+                <div className='nav-speech-bubble normal-font f500'>
+                    Home
+                    <AiOutlineCaretRight className='speech-triangle'/>
+                </div>
                 <img className='white-logo' alt='Discord Logo' src={logo}/>
             </NavLink>
             <div className='line'>
 
             </div>
-            <section className='nav-circle light-gray-back'>
+            <section className='nav-circle plus-server light-gray-back'>
                 <div className='white-strip'></div>
-                <div className='nav-speech-bubble normal-font f500'>Add a Server</div>
+                <div className='nav-speech-bubble normal-font f500'>
+                    Add a Server
+                    <AiOutlineCaretRight className='speech-triangle'/>
+                    </div>
                 <IoAddSharp className='green-icon'/>
             </section>
             <NavLink exact activeClassName='active-green' to='/guild-discovery' className='nav-circle light-gray-back'>
                 <section className='white-strip'></section>
-                <div className='nav-speech-bubble normal-font f500'>Explore Public Servers</div>
+                <div className='nav-speech-bubble normal-font f500'>
+                    Explore Public Servers
+                    <AiOutlineCaretRight className='speech-triangle'/>
+                    </div>
                 <IoCompassSharp className='green-icon'/>
             </NavLink>
         </div>
