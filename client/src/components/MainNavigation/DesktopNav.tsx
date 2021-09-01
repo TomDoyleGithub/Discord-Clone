028f2e1cfef5bc7ca8a39e4eefa172a26266bf60
Punkinut <heypunkinut@gmail.com>
2021-09-01 11:49:29 +1000

    Added icons in desktop nav

--- client/src/components/MainNavigation/DesktopNav.tsx
+++ client/src/components/MainNavigation/DesktopNav.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './navigation.scss';
 import Auth from '../../utils/auth';
+import { IoAddSharp, IoCompassSharp } from 'react-icons/io5';
+import logo from '../../images/Discord-White.svg';
 
 
 function DesktopNav() {
@@ -11,18 +13,21 @@ function DesktopNav() {
         <div className='border left-side-nav-container'>
             <section className='nav-circle purple-back'>
                 <div className='white-strip'></div>
-                <div className='nav-speech-bubble'></div>
+                <div className='nav-speech-bubble normal-font f500'>Home</div>
+                <img className='white-logo' alt='Discord Logo' src={logo}/>
             </section>
             <div className='line'>
 
             </div>
             <section className='nav-circle light-gray-back'>
                 <div className='white-strip'></div>
-
+                <div className='nav-speech-bubble normal-font f500'>Add a Server</div>
+                <IoAddSharp className='green-icon'/>
             </section>
             <section className='nav-circle light-gray-back'>
                 <div className='white-strip'></div>
-
+                <div className='nav-speech-bubble normal-font f500'>Explore Public Servers</div>
+                <IoCompassSharp className='green-icon'/>
             </section>
         </div>
     )
