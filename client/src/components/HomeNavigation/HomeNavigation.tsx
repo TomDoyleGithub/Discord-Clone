371a6fbbd70387121ebb11243fa8a6d6ce123f46
Punkinut <heypunkinut@gmail.com>
2021-09-03 14:14:19 +1000

    Added stage discovery logo

--- client/src/components/HomeNavigation/HomeNavigation.tsx
+++ client/src/components/HomeNavigation/HomeNavigation.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './navSide.scss';
 import friend from '../../images/Friends.svg';
+import discovery from '../../images/Discover-Icon.svg';
 import StartConversation from './StartConversation';
 import { NavLink, useLocation } from 'react-router-dom';
 import Auth from '../../utils/auth';
@@ -27,7 +28,7 @@ function HomeNavigation() {
                 <p className='normal-font'>Friends</p>
             </NavLink>
             <NavLink to='/discovery' activeClassName='friends-active' className='friends-tab' style={{ marginTop: '3px'}}>
-                <img className='friend-icon' src={friend} alt='Friend Icon'/>
+                <img className='discover-icon' src={discovery} alt='Friend Icon'/>
                 <p className='normal-font'>Stage Discovery</p>
             </NavLink>
         </section>
