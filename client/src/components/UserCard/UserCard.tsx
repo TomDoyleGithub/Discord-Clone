cccff4f6a936ced17a9bf19fc9c9c42927644dcb
Punkinut <heypunkinut@gmail.com>
2021-09-04 13:44:17 +1000

    Setup basic copy function

--- client/src/components/UserCard/UserCard.tsx
+++ client/src/components/UserCard/UserCard.tsx
@@ -4,15 +4,19 @@ import ProPic from '../StandardProPic/ProPic';
 import './userCard.scss';
 
 function UserCard() {
+    const handleCopy = () => {
+        // This will copy the data when implemented
+        navigator.clipboard.writeText('Punkinut');
+    };
     if (!Auth.loggedIn()) {
         return <></>
     }
     return (
         <section className='usercard-container'>
             <ProPic />
-            <div>
-                <p className='username'>Punkinut</p>
-                <p>#4681</p>
+            <div className='card-info-container' onClick={handleCopy}>
+                <p className='username header-font f700'>Punkinut</p>
+                <p className='user-code normal-font'>#4681</p>
             </div>
         </section>
     )
