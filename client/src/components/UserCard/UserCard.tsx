4b00d7abb1095334646780b37fcacdaccf22e724
Punkinut <heypunkinut@gmail.com>
2021-09-05 10:32:12 +1000

    Added hover effects to user card icons

--- client/src/components/UserCard/UserCard.tsx
+++ client/src/components/UserCard/UserCard.tsx
@@ -9,11 +9,11 @@ import headsetOff from '../../images/Head-Off.svg';
 import './userCard.scss';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { UPDATE_DEAFEN, UPDATE_MUTE } from '../../redux/actions';
+import { AiOutlineCaretRight } from 'react-icons/ai';
 
 function UserCard() {
     const dispatch = useDispatch();
-    const state = useSelector((state: RootStateOrAny) => state);
-    const {mute, deafen} = state;
+    const { mute, deafen } = useSelector((state: RootStateOrAny) => state);
 
     const handleCopy = () => {
         // This will copy the data when implemented
@@ -35,22 +35,35 @@ function UserCard() {
         <section className='usercard-container'>
             <ProPic />
             <div className='card-info-container' onClick={handleCopy}>
+                <section className='user-bubble normal-font f500'>Click to copy username<AiOutlineCaretRight className='user-triangle' style={{right: '80px'}}/></section>
                 <p className='username header-font f700'>Punkinut</p>
                 <p className='user-code normal-font'>#4681</p>
             </div>
             <div className='user-icon-container'> 
                 <div onClick={handleMute}>
                     {!mute ? (
+                        <>
+                        <section className='user-bubble normal-font f500'>Mute<AiOutlineCaretRight className='user-triangle' style={{right: '20px'}}/></section>
                         <img src={mic} alt='Mic Icon' style={{width: '12px'}}/>
+                        </>
                     ) : (
+                        <>
+                        <section className='user-bubble normal-font f500'>Unmute<AiOutlineCaretRight className='user-triangle' style={{right: '28px'}}/></section>
                         <img src={micOff} alt='Mic Icon' style={{width: '15px', position: 'relative', left: '0.45px'}}/>
+                        </>
                     )}
                 </div>
                 <div onClick={handleDeafen}>
                     {!deafen ? (
+                        <>
+                        <section className='user-bubble normal-font f500'>Deafen<AiOutlineCaretRight className='user-triangle' style={{right: '26px'}}/></section>
                         <img src={headset} alt='Mic Icon' style={{width: '17px'}}/>
+                        </>
                     ) : (
+                        <>
+                        <section className='user-bubble normal-font f500'>Undeafen<AiOutlineCaretRight className='user-triangle' style={{right: '34px'}}/></section>
                         <img src={headsetOff} alt='Mic Icon' style={{width: '17px'}}/>
+                        </>
                     )}
                 </div>
                 <div>
