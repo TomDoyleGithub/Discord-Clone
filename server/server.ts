ffbc65172cd0b0fca12839c2efe07f5fc75b97cb
Punkinut <heypunkinut@gmail.com>
2021-09-08 16:32:41 +1000

    ...

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,9 @@ import authMiddleware from './utils/auth';
 
 import db from './config/connection';
 
+const isProd = process.env.NODE_ENV === 'production';
+let basePath = '../';
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -20,13 +23,15 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+if (isProd) {
+    basePath = '../../'
+    app.use(express.static(path.join(__dirname, basePath + 'client/build')));
 };
+
 
 app.get('*', (req:any,res:any) => {
     console.log(req.query)
-   return res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+   return res.sendFile(path.join(__dirname, basePath + 'client/build/index.html'));
 });
 
 db.once('open', () => {
