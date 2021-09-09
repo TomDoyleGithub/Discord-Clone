e3d9651a93ec56ca38ec7852a2709426bdbe848e
Punkinut <heypunkinut@gmail.com>
2021-09-09 14:49:46 +1000

    Trying to fix socket

--- client/src/utils/Socket.tsx
+++ client/src/utils/Socket.tsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io('ws://localhost:4000');
+const socket = io('http://localhost:3001' || '/');
 
 export default socket;
--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,10 @@ const isProd = process.env.NODE_ENV === 'production';
 let basePath = '../';
 
 const app = express();
+
+const saServer = require('http').Server(app);
+const io = require('socket.io')(saServer);
+
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
@@ -40,3 +44,8 @@ db.once('open', () => {
         console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
 });
+
+io.on("connection", (socket:any) => {
+    console.log('A user connected');
+    io.emit('Welcome', 'Socket server is working!')
+  });
