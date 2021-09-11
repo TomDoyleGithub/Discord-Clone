ea178e12c3f52428f1734a888047b520b1baf2a2
Punkinut <heypunkinut@gmail.com>
2021-09-11 11:01:58 +1000

    Add idle status effect

--- client/src/components/Status.tsx
+++ client/src/components/Status.tsx
@@ -1,13 +1,36 @@
 import React from 'react'
-import { RootStateOrAny, useSelector } from 'react-redux';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import Online from './StatusIcons/Online';
 import Idle from './StatusIcons/Idle';
 import Disturb from './StatusIcons/Disturb';
 import Invisible from './StatusIcons/Invisible';
+import { useIdleTimer } from 'react-idle-timer';
+import { CHANGE_STATUS } from '../redux/actions';
 
 function Status({dataStatus}) {
+    const dispatch = useDispatch();
+
     let status = '';
     const { status: stateStatus } = useSelector((state: RootStateOrAny) => state);
+
+    const handleOnIdle = event => {
+        if (status === 'online') {
+            dispatch({ type: CHANGE_STATUS, status: 'idle' });
+        }
+      }
+    
+    const handleOnActive = event => {
+        if (dataStatus === 'online') {
+            dispatch({ type: CHANGE_STATUS, status: 'online' });
+        }
+    }
+      
+    useIdleTimer({
+      timeout: 5000,
+      onIdle: handleOnIdle,
+      onActive: handleOnActive,
+      debounce: 500
+    });
 
     // Changes the definition of status depending on the redux state
     if ( stateStatus === '') {
