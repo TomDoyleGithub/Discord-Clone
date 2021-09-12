9be850500592b3b6f7518ba4d8b9f72916b9fd5b
Punkinut <heypunkinut@gmail.com>
2021-09-12 14:34:50 +1000

    Added basic friends nav component

---
+++ client/src/components/FriendsNav/FriendsNav.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './friendsNav.scss'
+
+function FriendsNav() {
+    return (
+        <section className='friend-nav-container'>
+            <section className='friend-choice'>
+                
+            </section>
+        </section>
+    )
+}
+
+export default FriendsNav
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeNavigation from '../components/HomeNavigation/HomeNavigation'
+import FriendNav from '../components/FriendsNav/FriendsNav'
 
 function Home() {
     return (
         <>
             <HomeNavigation/>
+            <FriendNav/>
         </>
     )
 }
