922bba75316602cda9c6b88e576d15a735b1673c
Punkinut <heypunkinut@gmail.com>
2021-09-13 12:39:44 +1000

    Added text to basic wumpus pages

--- client/src/components/NoOneDisplay/NoOneDisplay.tsx
+++ client/src/components/NoOneDisplay/NoOneDisplay.tsx
@@ -11,24 +11,28 @@ function NoOneDisplay() {
         return (
             <article>
                 <img src={sleepWumpus} alt='Asleep Wumpus'/>
+                <p className='wumpus-text normal-font f300'>No one's around to play with Wumpus.</p>
             </article>
         )
     } else if (friendsNav === 'all') {
         return (
             <article>
                 <img src={playWumpus} alt='Play Wumpus'/>
+                <p className='wumpus-text normal-font f300'>Wumpus is waiting on friends. You don’t have to though!</p>
             </article>
         )
     } else if (friendsNav === 'pending') {
         return (
             <article>
                 <img src={leafWumpus} alt='Leaf Wumpus'/>
+                <p className='wumpus-text normal-font f300'>There are no pending friend requests. Here's Wumpus for now.</p>
             </article>
         )
     } else if (friendsNav === 'blocked') {
         return (
             <article>
                 <img src={blockWumpus} alt='Block Wumpus'/>
+                <p className='wumpus-text normal-font f300'>You can't unblock the Wumpus.</p>
             </article>
         )
     }
