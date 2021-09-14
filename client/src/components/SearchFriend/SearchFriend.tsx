188c7c5643a5d0610c8f669528be476998fe90ec
Punkinut <heypunkinut@gmail.com>
2021-09-14 11:05:40 +1000

    Designed button that allows you to send a friend request

--- client/src/components/SearchFriend/SearchFriend.tsx
+++ client/src/components/SearchFriend/SearchFriend.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import './searchFriends.scss'
 
 function SearchFriend() {
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Submitted!')
+    };
+
     return (
         <section className='search-friend-container'>
             <section className='sub-friend-container'>
                 <p className='normal-font f600 header-add-friend'>Add Friend</p>
                 <p className='normal-font subby-add-friend'>You can add a friend with their Discord Tag. It's cAsE sEnSitIvE!</p>
                 <input autoFocus className='add-friend-input normal-font' placeholder='Enter a Username#0000'></input>
+                <form onSubmit={handleFormSubmit}>
+                    <button className='send-friend-button normal-font' type='submit'>Send Friend Request</button>
+                </form>
             </section>
         </section>
     )
