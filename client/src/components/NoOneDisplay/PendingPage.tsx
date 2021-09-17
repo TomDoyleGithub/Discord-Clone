d6e16d363e12c1960f2d5cd656a56a2a58027626
Punkinut <heypunkinut@gmail.com>
2021-09-17 12:44:45 +1000

    Cleaned up jsx and add pending request icons

--- client/src/components/NoOneDisplay/PendingPage.tsx
+++ client/src/components/NoOneDisplay/PendingPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProPic from '../StandardProPic/ProPic';
+import { IoCheckmarkSharp, IoCloseSharp } from "react-icons/io5";
 
 function PendingPage({ pendingLength, pendingResults }) {
     return (
@@ -7,7 +8,7 @@ function PendingPage({ pendingLength, pendingResults }) {
             <section className='sub-friend-card-container'>
                 <p className='pending-label normal-font f500'>Pending — {pendingLength}</p>
                 {pendingResults.map((user) => (
-                    user.status === 1 ? (
+                    
                         <section className='pendly-card' key={user.user._id}>
                             <section>
                                 <ProPic />
@@ -21,40 +22,25 @@ function PendingPage({ pendingLength, pendingResults }) {
                                 <span className='request-card-username header-font f700'>{user.user.username.slice(0, -5)}</span>
                                 <span className='normal-font request-card-code'>{user.user.username.slice(Math.max(user.user.username?.length - 5, 0))}</span>
                             </p>
-                            <p className='normal-font request-card-subby'>Outgoing Friend Request</p>
+                            {user.status === 1 ? (
+                                <p className='normal-font request-card-subby'>Outgoing Friend Request</p>
+                            ) : (
+                                <p className='normal-font request-card-subby'>Incoming Friend Request</p>
+                            )}
                             </section>
                             <section className='choose-request-container'>
+                            {user.status === 1 ? (
                                 <section>
-
+                                    <IoCheckmarkSharp className='pending-icon'/>
                                 </section>
+                            ) : (
+                                <></>
+                            )}
                                 <section>
-                                    
+                                    <IoCloseSharp className='pending-icon'/>
                                 </section>
                             </section>
                         </section>
-                    ) : (
-                        <section className='pendly-card' key={user.user._id}>
-                            <section>
-                                <ProPic />
-                                <section className='invisible real-status request-status'>
-                                    <section className='hole-dot'></section>
-                                </section>
-                            </section>
-                            
-                            <section>
-                            <p>
-                                <span className='request-card-username header-font f700'>{user.user.username.slice(0, -5)}</span>
-                                <span className='normal-font request-card-code'>{user.user.username.slice(Math.max(user.user.username?.length - 5, 0))}</span>
-                            </p>
-                            <p className='normal-font request-card-subby'>Incoming Friend Request</p>
-                            </section>
-                            <section className='choose-request-container'>
-                                <section>
-
-                                </section>
-                            </section>
-                        </section>
-                    )
                 ))}
             </section>
         </section>
