ce5e660ba161d694e148de371d608a4b15d0d56c
Punkinut <heypunkinut@gmail.com>
2021-09-22 10:38:01 +1000

    Added discover header along with a basic card

--- client/src/components/GuildNavigation/GuildNavigation.tsx
+++ client/src/components/GuildNavigation/GuildNavigation.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './guildNav.scss'
+import { IoCompassSharp } from 'react-icons/io5';
 
 function GuildNavigation() {
     return (
         <section className='guild-nav-container'>
-           
+            <p className='discover-header header-font f700'>Discover</p>
+            {/* Turn this card into a seperate component and pass the values in a props */}
+           <section className='friends-tab'>
+                <IoCompassSharp className='compass-icon'/>
+                <p className='normal-font'>Home</p>
+           </section>
         </section>
     )
 }
