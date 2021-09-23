7e96e2f29bd6fb26520ba20a8f3e43ae97d2e19e
Punkinut <heypunkinut@gmail.com>
2021-09-23 17:20:40 +1000

    User card tool tip pops up but it still has some bugs

--- client/src/components/NoOneDisplay/CardModal/CardModal.tsx
+++ client/src/components/NoOneDisplay/CardModal/CardModal.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function CardModal() {
+function CardModal({id, state}) {
     return (
-        <section className='card-settings normal-font'>
+        <section  className={'card-settings normal-font ' + (id === state ? 'card-settings-show' : 'hidey-time')}>
             <p className='mini-sub-card-modal greyby-card'>Start Video Call</p>
             <p className='mini-sub-card-modal greyby-card'>Start Voice Call</p>
             <p className='mini-sub-card-modal reddy-card'>Remove Friend</p>
--- client/src/components/NoOneDisplay/ContactIcons.tsx
+++ client/src/components/NoOneDisplay/ContactIcons.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoChatbox, IoEllipsisVerticalSharp } from "react-icons/io5";
 import CardModal from './CardModal/CardModal'
 function ContactIcons({user}) {
+    const [thisState, setThisState] = useState('');
+    const pageStatusEvent = () => {
+        setThisState('');
+        window.removeEventListener('click', pageStatusEvent);
+    }
+    const handleClick = (e) => {
+        e.stopPropagation();
+        const id = e.currentTarget.getAttribute('data-value');
+        setThisState('');
+        setThisState(id);
+        window.addEventListener('click', pageStatusEvent);
+    };
+
     return (
         <section className='choose-request-container'>
             <section data-value={user.user._id} className='ticky-one'>
                 <aside className='pending-tooltip normal-font f500 ticky-one-accept'>Message</aside>
                 <IoChatbox className='message-friend-icon'/>
             </section>
-            <section data-value={user.user._id} className='ticky-two'>
+            <section onClick={handleClick}  data-value={user.user._id} className='ticky-two'>
                 <aside className='pending-tooltip normal-font f500 ticky-two-ignore'>More</aside>
                 <IoEllipsisVerticalSharp className='message-friend-icon' />
-                <CardModal/>
+                <CardModal id={user.user._id} state={thisState}/>
             </section>
         </section>
     )
