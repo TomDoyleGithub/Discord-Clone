bbba5b8eff6819719d5cce1524144b6b5d50bb48
Punkinut <heypunkinut@gmail.com>
2021-09-27 14:26:28 +1000

    Changed the emoji effect for speed

--- client/src/components/CustomStatusModal/CustomStatusModal.tsx
+++ client/src/components/CustomStatusModal/CustomStatusModal.tsx
@@ -4,18 +4,10 @@ import { TOGGLE_CUSTOM_STATUS } from '../../redux/actions';
 import Wumpus from '../../images/Status-Wumpus.svg'
 import './statusModal.scss'
 import { IoCloseOutline } from "react-icons/io5";
-import defaultEmoji from '../../images/emoji-default.svg';
-import { e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18 } from '../../images'
+import { emojiArr } from '../../utils/emojiArr';
 
 function CustomStatusModal() {
-    const [realChoice, setEmoji] = useState(defaultEmoji)
-    const emojiArr = [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18];
-
-    const changeEmoji = () => {
-        const random = Math.floor(Math.random() * emojiArr.length);
-        setEmoji(emojiArr[random])
-    };
-
+    const [realChoice, setEmoji] = useState('0px 0px')
     const dispatch = useDispatch();
     const hideModal = (e) => {
         if(e.target === e.currentTarget) {
@@ -23,6 +15,12 @@ function CustomStatusModal() {
          }
     };
     const { customStatusModal  } = useSelector((state: RootStateOrAny) => state);
+
+    const changeEmoji = () => {
+        const random = Math.floor(Math.random() * emojiArr.length);
+        setEmoji(emojiArr[random])
+    };
+
     return (
         <div onClick={hideModal} className={'modal-container ' + (customStatusModal ? 'show' : 'hide')}>
             <section className='password-send custom-send'>
@@ -35,7 +33,7 @@ function CustomStatusModal() {
                     <section className='input-container'>
                             <label className='normal-font f500 status-label'>What's cookin', woolywowo?</label>
                             <div className='fake-input-status'>
-                                <img className='select-emoji'  alt='default-emoji-icon' onMouseEnter={changeEmoji} src={realChoice}/>
+                                <div onMouseEnter={changeEmoji} className='emoji-sprite' style={{backgroundPosition: realChoice}}></div>
                                 <input type='name' name='CustomStatus' maxLength={30} placeholder='Support has arrived'  className='normal-font f300 real-input-status'/>
                             </div>
                     </section>
