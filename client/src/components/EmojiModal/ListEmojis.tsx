695a2d10f9a6e30d3c78dc7005e711bd563b415b
Punkinut <heypunkinut@gmail.com>
2021-10-01 16:19:21 +1000

    Cleaned up ListEmojis component

--- client/src/components/EmojiModal/ListEmojis.tsx
+++ client/src/components/EmojiModal/ListEmojis.tsx
@@ -7,6 +7,7 @@ import { foodArr } from '../../utils/emojiArrays/foodArr';
 import { activityArr } from '../../utils/emojiArrays/activityArr';
 import { travelArr } from '../../utils/emojiArrays/travelArr';
 import { objectsArr } from '../../utils/emojiArrays/objectsArr';
+import { symbolsArr } from '../../utils/emojiArrays/symbolsArr';
 
 function ListEmojis({ type }) {
     const dispatch = useDispatch();
@@ -16,42 +17,20 @@ function ListEmojis({ type }) {
         dispatch({ type: SET_EMOJI_PLACEHOLDER, emojiPlaceholder: name });
     };
     if (type === 'people') {
-        return (
-            <>
-                {peopleArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}
-            </>
-        )
+        return <>{peopleArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
     } else if (type === 'nature') {
-        return (
-            <>
-                {natureArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}
-            </>
-        )
+        return <>{natureArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
     } else if (type === 'food') {
-        return (
-            <>
-                {foodArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}
-            </>
-        )
+        return <>{foodArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
     } else if (type === 'activities') {
-        return (
-            <>
-                {activityArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}
-            </>
-        )
+        return <>{activityArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
     } else if (type === 'travel') {
-        return (
-            <>
-                {travelArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}
-            </>
-        )
-    }  else if (type === 'objects') {
-        return (
-            <>
-                {objectsArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}
-            </>
-        )
-    }  else {
+        return <>{travelArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
+    } else if (type === 'objects') {
+        return  <>{objectsArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
+    } else if (type === 'symbols') {
+        return <>{symbolsArr.map((emoji) => (<div key={emoji.dim} onMouseEnter={handleHover} data-value={emoji.name} className='single-emoji-container'><div className='single-emoji' style={{backgroundPosition: emoji.dim}}></div></div>))}</>
+    } else {
         return <></>
     }
 }
