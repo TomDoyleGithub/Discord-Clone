a75545753da891e2f937cad856b487d87b4a4336
Punkinut <heypunkinut@gmail.com>
2021-10-01 11:54:53 +1000

    Added all the nature emojis

--- client/src/components/EmojiModal/OfficialEmojiSelect.tsx
+++ client/src/components/EmojiModal/OfficialEmojiSelect.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import PeopleEmojis from './PeopleEmojis'
+import NatureEmojis from './NatureEmojis'
 
 function OfficialEmojiSelect({ type }) {
     if (type === 'people') {
         return (
             <section className='offical-emoji-selection'>
                 <PeopleEmojis/>
+            </section> 
+        )
+    } else if (type === 'nature') {
+        return (
+            <section className='offical-emoji-selection'>
+                <NatureEmojis/>
             </section> 
         )
     } else {
