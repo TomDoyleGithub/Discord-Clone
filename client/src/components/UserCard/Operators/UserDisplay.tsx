1d55b00162901549be2e8b9e8173ba603c4422b1
Punkinut <heypunkinut@gmail.com>
2021-10-11 13:22:31 +1100

    Cleaned up methods on UserDisplay file

--- client/src/components/UserCard/Operators/UserDisplay.tsx
+++ client/src/components/UserCard/Operators/UserDisplay.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import emoji from 'react-easy-emoji';
 
 function UserDisplay({username, customStatus, me}) {
+
+    const slicedUsername = me?.username?.slice(Math.max(me?.username?.length - 5, 0));
+    const longUsername = `${username?.slice(0, 9)}...`;
+    const emojiDisplay = emoji(`${customStatus?.replace(/~/g, ' ')}`);
+
     return (
         <>
             { username?.length > 8 ? (
-                <p className='username header-font f700'>{`${username?.slice(0, 9)}...`}</p>
+                <p className='username header-font f700'>{longUsername}</p>
             ) : (
                 <p className='username header-font f700'>{username}</p>
             )}
             
             {customStatus === '' || customStatus === '~' ? (
-                <p className='user-code normal-font'>{me?.username?.slice(Math.max(me?.username?.length - 5, 0))}</p>
+                <p className='user-code normal-font'>{slicedUsername}</p>
             ) : (
                 <div className='custom-status-usercard'>
-                    <p className='user-code normal-font'>{emoji(`${customStatus?.replace(/~/g, ' ')}`)}</p>
-                    <p className='user-code normal-font'>{me?.username?.slice(Math.max(me?.username?.length - 5, 0))}</p>
+                    <p className='user-code normal-font'>{emojiDisplay}</p>
+                    <p className='user-code normal-font'>{slicedUsername}</p>
                 </div>
             )}
         </>
